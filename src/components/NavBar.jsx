169dfce99e8dfbcd9069627999e763eb866963be
Akash Das <akashdas02052@gmail.com>
2026-05-30 11:53:19 +0530

    Fix: Fix navbar style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,11 @@
+import { Link } from "react-router-dom"
 import styles from "../style_modules/component_modules/NavBar.module.css"
 export default function navBar(prop) {
   const { setIsMenuBtnClicked } = prop
   return (
     <>
       <div className={`${styles.navbar}`}>
-        <div className={`d-none d-sm-block ${styles.name_container}`}>
-          <h2 className={`${styles.name}`}>Anvaya CRM</h2>
-          <h5 className={`${styles.name_description}`}>
-            Agent Performance Monitor
-          </h5>
-        </div>
+        <div className="d-flex gap-3 mt-2 mt-sm-0">
         <button
           className={`${styles.menu_button}`}
           title="Menu"
@@ -17,13 +13,20 @@ export default function navBar(prop) {
         >
           <i className="bi bi-list"></i>
         </button>
-        <div className={`${styles.nav_items}`}>
+        <div className={`${styles.name_container}`}>
+          <h2 className={`${styles.name}`}>Anvaya CRM</h2>
+          <h5 className={`${styles.name_description}`}>
+            Agent Performance Monitor
+          </h5>
+        </div>
+        </div>
+        <div className={`${styles.nav_items} mb-2 mb-sm-0 mt-0 mt-sm-2`}>
           {/* <button className={`${styles.nav_btn}`}>
             <i className={`bi bi-moon-fill ${styles.icon}`}></i>
           </button> */}
-          {/* <button className={`${styles.nav_btn}`}>
+          <Link to="/settings" className={`${styles.nav_btn}`}>
             <i className={`bi bi-gear ${styles.icon}`}></i>
-          </button> */}
+          </Link>
           {/* <button className={`${styles.nav_btn}`}>
             <i className={`bi bi-person-fill ${styles.icon}`}></i>
           </button> */}
